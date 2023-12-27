ca5ec30c505684e299c67619db2cd4e2bf4b1bac
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-12-27 22:47:07 +0630

    just testing database relationship

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,28 @@ Route::get('/post/delete/{id}', [PostController::class, 'postDelete'])->name('po
 Route::get('/post/updatePage/{id}', [PostController::class, 'postUpdate'])->name("post#update");
 Route::get('/post/editPage/{id}', [PostController::class, 'editPage'])->name("post#edit");
 Route::post('/post/update', [PostController::class, 'update'])->name("post#updateNow");
+
+
+// database relation test
+Route::get('product/list', function(){
+    $data = Product::select('*')
+            ->join('categories', 'products.category_id', 'categories.id')
+            ->get();
+    dd($data);
+});
+
+// database relation test
+Route::get('product/list', function(){
+    $data = Product::select('*')
+            ->rightJoin('categories', 'products.category_id', 'categories.id')
+            ->get();
+    dd($data);
+});
+
+// database relation test
+Route::get('product/list', function(){
+    $data = Product::select('*')
+            ->leftJoin('categories', 'products.category_id', 'categories.id')
+            ->get();
+    dd($data);
+});
